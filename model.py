6277b7efa531cbba45574189c202dd7ebefd332f
saket kumar <kumarsaket23031@gmail.com>
2025-12-17 01:11:37 +0530

    initial model

---
+++ model.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.optimize import minimize
+# i wrote parameters form the numerical exapmle in page 12
+m=20
+b=0.5
+c=4
+c_prime=4.5
+delta=0.3
+A0=500
+A1=6.5
+A2 = 6      
+A3 = 30     
+A4 = 12    
+A5 = 8      
+A6 = 3
+p = 60               
+xi = 5               
+theta0 = 0.2         
+a1 = 0.5
+qs = 0.9            
+A = 30
+nu = 0.4
+g = 10
+alpha=0.2
+gamma=0.5
+T=1.0
+#qr = (1 − e−a1qs). PAGE 5 Assumption 1
+qr = 1 - np.exp(-a1 * qs)
+
+# θ = θoe−a1ξwhere 0 < θ0 < 1 and a1 > 0 assumption 3 page 5
+theta = theta0 * np.exp(-a1 * xi)
+
+def exponential_term(t1):
+    return np.exp((theta + b * A**nu) * t1)
+
+# profit function
+def total_profit(t1):
+    
+
+    # Constraint: 0 < t1 < T
+    if t1 <= 0 or t1 >= T:
+        return 1e6
+
+    # Common demand terms
+    term1 = (m*p - delta + c*qr) * A**nu
+    term2 = (m*p - delta + c_prime*qr) * A**nu
+
+    exp_val = exponential_term(t1)
+
+    
+    holding_deterioration = (A1 + A2*theta) * term1 / (theta + b*A**nu)**2 * \
+        (exp_val - 1 - (theta + b*A**nu)*t1)
+
+    
+    shortage_cost = A4 * term2 * (T - t1)**2 / 2
+
+    
+    rework_screening_cost = (A5*alpha + A6) * term1 / ((theta + b*A**nu)*(1 - alpha)) * \
+        (exp_val - 1)
+
+    
+    purchase_cost = A3 * (
+        term1/(theta + b*A**nu)*(exp_val - 1)
+        + term2*(T - t1)
+        + gamma*alpha*term1/((theta + b*A**nu)*(1 - alpha))*(exp_val - 1)
+    )
+
+    
+    sales_revenue = p * (
+        (c - c_prime)*qr*A**nu*t1
+        + b*term1*A**nu/(theta + b*A**nu)**2 *
+        (exp_val - 1 - (theta + b*A**nu)*t1)
+        + term2*T
+    )
+
+    # Total Profit
+    profit = (
+        sales_revenue
+        - A0
+        - holding_deterioration
+        - shortage_cost
+        - rework_screening_cost
+        - purchase_cost
+        - g*A
+        - xi*T
+    )
+
+    return -profit / T   # negative for minimization as scipy minimizes
+
+#working on optimisation
+result = minimize(
+    total_profit,
+    x0=[0.8],
+    bounds=[(0.01, T - 0.01)]
+)
+
+t1_star = result.x[0]
+profit_star = -result.fun
+
+# results obtained
+print("Optimal replenishment time t1* =", round(t1_star, 4))
+print("Optimal total profit =", round(profit_star, 2))
